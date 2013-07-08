ced4a8a8255f3c2dd534d74e5a976f5a32bccea8
Jeremy Jusak <kashll@comcast.net>
2013-07-07 21:17:42 -0700

    Basic code for implementing INotifyPropertyChanged

---
+++ src/Utility/NotifyPropertyChangedImpl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Utility
+{
+    public abstract class NotifyPropertyChangedImpl : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged
+        {
+            add { m_propertyChangedEventHandler += value; }
+            remove { m_propertyChangedEventHandler -= value; }
+        }
+
+        public void RaisePropertyChanged(params string[] propertyNames)
+        {
+            foreach (string propertyName in propertyNames)
+                RaisePropertyChanged(propertyName);
+        }
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = m_propertyChangedEventHandler;
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = m_propertyChangedEventHandler;
+
+            if (handler != null) 
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetPropertyField<T>(string propertyName, T value, ref T field)
+        {
+            if (EqualityComparer<T>.Default.Equals(value, field))
+                return false;
+
+            field = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        PropertyChangedEventHandler m_propertyChangedEventHandler;
+    }
+}
