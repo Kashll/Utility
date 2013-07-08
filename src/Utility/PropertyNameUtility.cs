747906103eb7f9d68c28d9fc33402eb3c40866fe
Jeremy Jusak <kashll@comcast.net>
2013-07-07 20:04:37 -0700

    Add utilities for string Format and getting Property Names.

---
+++ src/Utility/PropertyNameUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Utility
+{
+    public static class PropertyNameUtility
+    {
+        /// <summary>
+        /// Uses reflection to get the name of the property for the given expression
+        /// </summary>
+        public static string GetPropertyName<TProperty>(Expression<Func<TProperty>> propertyExpression)
+        {
+            MemberExpression memberExpression = propertyExpression.Body as MemberExpression;
+            if (memberExpression != null)
+                return memberExpression.Member.Name;
+
+            throw new InvalidOperationException("Couldn't get property name for {0}".FormatInvariant(propertyExpression));
+        }
+    }
+}
---
+++ src/Utility/StringUtility.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace Utility
+{
+    public static class StringUtility
+    {
+        /// <summary>
+        /// Extension method for a given format string and arguments using invariant culture
+        /// </summary>
+        public static string FormatInvariant(this string formatString, params object[] args)
+        {
+            return string.Format(CultureInfo.InvariantCulture, formatString, args);
+        }
+    }
+}
